d014fed99e7eef39982367fb32689d29aa7eca19
kmilek <kmilek73@gmail.com>
2022-12-05 13:03:51 +0100

    rozwiązanie

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+let zmienna_1 = 12;
+
+
+if (zmienna_1>10){
+console.log("zmienna jest większa od 10");
+alert("zmienna jest większa od 10");
+
+}
+else {
+    console.log("zmienna jest mniejsza od 10");
+    alert("zmienna jest mniejsza od 10");
+}
+
